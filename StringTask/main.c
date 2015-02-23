a44a8cc161f776ee4e002edfa8765208dad7ab99
Aleksey Lebedev <leb-al@outlook.com>
2015-02-23 03:58:18 +0300

    All task done

--- StringTask/main.c
+++ StringTask/main.c
@@ -3,6 +3,8 @@
 #include <ctype.h>
 #include <string.h>
 
+const int LEXEMES_COUNT = 5;
+
 int checkNull(char *pointer) {
     if (pointer == NULL) {
         printf("Wrong memory alloc.\n");
@@ -13,13 +15,25 @@ int checkNull(char *pointer) {
 
 int errorCode = 0;
 int currentSymbol = 0;
+int wasInLexeme = 0;
+int lastWasLexeme = 0;
 
 void readSymbol() {
     currentSymbol = getchar();
 }
 
+int checkIsBeginLexeme(char **lexemes, size_t lexemesCount) {
+    wasInLexeme = 0;
+    for (size_t i = 0; i < lexemesCount; ++i) {
+        if (((char) currentSymbol) == lexemes[i][0]) {
+            wasInLexeme = 1;
+            return 0;
+        }
+    }
+    return 1;
+}
+
 char *readToken(char **lexemes, size_t lexemesCount) {
-    int inQuotes = 0;
     size_t capacity = 10;
     size_t length = 0;
     char *line = (char *) malloc(capacity * sizeof(char));
@@ -28,8 +42,10 @@ char *readToken(char **lexemes, size_t lexemesCount) {
         return NULL;
     }
     line[0] = 0;
+    int begin = 0;
     if (currentSymbol == 0) {
         readSymbol();
+        begin = 1;
     }
     while (isspace(currentSymbol)) {
         readSymbol();
@@ -37,14 +53,15 @@ char *readToken(char **lexemes, size_t lexemesCount) {
     if (currentSymbol == EOF) {
         return NULL;
     }
-    int inWord = 1;
-    do {
-        if ((!inQuotes) && (currentSymbol == '"' || currentSymbol == '\'')) {
-            inQuotes = currentSymbol;
+    if (begin)
+        checkIsBeginLexeme(lexemes, lexemesCount);
+    if (wasInLexeme) {
+        if (lastWasLexeme) {
+            //It have to be empty word between two lexemes.
+            lastWasLexeme = 0;
         } else {
-            if (inQuotes && (currentSymbol == inQuotes)) {
-                inQuotes = 0;
-            } else {
+            int stillLexeme;
+            do {
                 line[length] = (char) currentSymbol;
                 ++length;
                 if (length == capacity) {
@@ -56,23 +73,64 @@ char *readToken(char **lexemes, size_t lexemesCount) {
                     }
                 }
                 line[length] = 0;
-            }
+                stillLexeme = 0;
+                for (size_t i = 0; i < lexemesCount; ++i) {
+                    if (strcmp(line, lexemes[i]) == 0) {
+                        stillLexeme = 1;
+                        break;
+                    }
+                }
+                if (stillLexeme)
+                    readSymbol();
+            } while (stillLexeme);
+            line[length - 1] = 0;
+            checkIsBeginLexeme(lexemes, lexemesCount);
+            if (wasInLexeme)
+                lastWasLexeme = 1;
         }
-        readSymbol();
-        if (currentSymbol == EOF) {
-            if (inQuotes) {
-                errorCode = 2;
-                printf("Wrong quote structure.\n");
-                return NULL;
+    } else {
+        int inQuotes = 0;
+        int inWord = 1;
+        do {
+            if ((!inQuotes) && (currentSymbol == '"' || currentSymbol == '\'')) {
+                inQuotes = currentSymbol;
             } else {
-                inWord = 0;
+                if (inQuotes && (currentSymbol == inQuotes)) {
+                    inQuotes = 0;
+                } else {
+                    line[length] = (char) currentSymbol;
+                    ++length;
+                    if (length == capacity) {
+                        capacity *= 2;
+                        line = (char *) realloc(line, capacity * sizeof(char));
+                        if (checkNull(line)) {
+                            errorCode = 1;
+                            return NULL;
+                        }
+                    }
+                    line[length] = 0;
+                }
             }
-        } else {
-            if ((!inQuotes) && isspace(currentSymbol)) {
-                inWord = 0;
+            readSymbol();
+            if (currentSymbol == EOF) {
+                if (inQuotes) {
+                    errorCode = 2;
+                    printf("Wrong quote structure.\n");
+                    return NULL;
+                } else {
+                    inWord = 0;
+                }
+            } else {
+                if (!inQuotes) {
+                    if (isspace(currentSymbol)) {
+                        inWord = 0;
+                    } else {
+                        inWord = checkIsBeginLexeme(lexemes, lexemesCount);
+                    }
+                }
             }
-        }
-    } while (inWord);
+        } while (inWord);
+    }
     return line;
 }
 
@@ -92,17 +150,28 @@ int addString(char *line) {
     return 0;
 }
 
-int cmpstring(const void *first, const void *second)
-{
-    return strcmp(* (char * const *) first, * (char * const *) second);
+int cmpstring(const void *first, const void *second) {
+    return strcmp(*(char *const *) first, *(char *const *) second);
 }
 
+void freeList(char **array, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        free(array[i]);
+    }
+    free(array);
+}
 
 int main() {
+    char **lexemes = malloc(LEXEMES_COUNT * sizeof(char *));
+    lexemes[0] = ";";
+    lexemes[1] = "&";
+    lexemes[2] = "&&";
+    lexemes[3] = "|";
+    lexemes[4] = "||";
     list = malloc(listCapacity * sizeof(char *));
     char *currentLine;
     do {
-        currentLine = readToken(NULL, 0);
+        currentLine = readToken(lexemes, LEXEMES_COUNT);
         if (currentLine) {
             if (addString(currentLine)) {
                 errorCode = 3;
@@ -111,14 +180,12 @@ int main() {
         }
     } while (currentLine);
     if (!errorCode) {
-        qsort(list, listSize, sizeof(char*), cmpstring);
+        qsort(list, listSize, sizeof(char *), cmpstring);
         for (int i = 0; i < listSize; ++i) {
             printf("\"%s\" \n", list[i]);
         }
     }
-    for (int i = 0; i < listSize; ++i) {
-        free(list[i]);
-    }
-    free(list);
+    freeList(list, listSize);
+    free(lexemes);
     return errorCode;
 }
