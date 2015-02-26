ee41960885308005c01d4dfb7adc85142d7c5bf1
Aleksey Lebedev <leb-al@outlook.com>
2015-02-26 18:46:31 +0300

    cat using FILE* functions

---
+++ Cat/catFILE.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+main(int argc, char **argv) {
+    for (int i = 1; i < argc; ++i) {
+        FILE *file = fopen(argv[i], "r");
+        if (file != NULL) {
+            size_t count = 0;
+            size_t size = 100;
+            char *text = malloc(size * sizeof(char));
+            if (text == NULL) {
+                fprintf(stderr, "Error in malloc\n");
+            } else {
+                do {
+                    count = fread(text, sizeof(char), size, file);
+                    for (int j = 0; j < count; ++j) {
+                        printf("%c", text[j]);
+                    }
+                } while (count > 0);
+                free(text);
+                fclose(file);
+            }
+        } else {
+            fprintf(stderr, "Can't open file '%s' with error %d\n", argv[i], errno);
+        }
+    }
+    return 0;
+}
