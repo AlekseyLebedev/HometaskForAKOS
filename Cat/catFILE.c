d207d1edc9e0601200e268a72017c5f09d8471d4
Aleksey Lebedev <leb-al@outlook.com>
2015-02-26 22:09:15 +0300

    Fix bugs in cat directories

--- Cat/catFILE.c
+++ Cat/catFILE.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 
 int main(int argc, char **argv) {
     for (int i = 1; i < argc; ++i) {
         FILE *file = fopen(argv[i], "r");
-        if (file != NULL) {
+        if (ferror(file) || (file == NULL)) {
+            perror(argv[i]);
+        } else {
             size_t count = 0;
-            size_t size = 100;
+            size_t size = 1024;
             char *text = malloc(size * sizeof(char));
             if (text == NULL) {
                 fprintf(stderr, "Error in malloc\n");
@@ -17,12 +18,14 @@ int main(int argc, char **argv) {
                     for (int j = 0; j < count; ++j) {
                         printf("%c", text[j]);
                     }
+                    if(ferror(file)){
+                        perror(argv[i]);
+                        break;
+                    }
                 } while (count > 0);
                 free(text);
                 fclose(file);
             }
-        } else {
-            fprintf(stderr, "Can't open file '%s' with error %d\n", argv[i], errno);
         }
     }
     return 0;
