0509473f43ac2b941c8e76230242d77075f3ed36
Aleksey Lebedev <leb-al@outlook.com>
2015-03-01 00:05:15 +0300

    Made optimizations and update cat

--- Cat/catFILE.c
+++ Cat/catFILE.c
@@ -2,31 +2,30 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    for (int i = 1; i < argc; ++i) {
-        FILE *file = fopen(argv[i], "r");
-        if (ferror(file) || (file == NULL)) {
-            perror(argv[i]);
-        } else {
-            size_t count = 0;
-            size_t size = 1024;
-            char *text = malloc(size * sizeof(char));
-            if (text == NULL) {
-                fprintf(stderr, "Error in malloc\n");
+    size_t size = 8192;
+    char *text = malloc(size * sizeof(char));
+    if (text == NULL) {
+        fprintf(stderr, "Error in malloc\n");
+    } else {
+        for (int i = 1; i < argc; ++i) {
+            FILE *file = fopen(argv[i], "r");
+            if ((file == NULL) || ferror(file)) {
+                perror(argv[i]);
             } else {
+                size_t count = 0;
                 do {
                     count = fread(text, sizeof(char), size, file);
-                    for (int j = 0; j < count; ++j) {
-                        printf("%c", text[j]);
-                    }
-                    if(ferror(file)){
+                    if (ferror(file)) {
                         perror(argv[i]);
                         break;
+                    } else {
+                        fwrite(text, sizeof(char), count, stdout);
                     }
                 } while (count > 0);
-                free(text);
                 fclose(file);
             }
         }
+        free(text);
     }
     return 0;
 }
--- Cat/catLowLevel.c
+++ Cat/catLowLevel.c
@@ -4,31 +4,30 @@
 #include <fcntl.h>
 
 int main(int argc, char **argv) {
-    for (int i = 1; i < argc; ++i) {
-        int file = open(argv[i], O_RDONLY);
-        if (file >= 0) {
-            ssize_t count = 0;
-            size_t size = 100;
-            char *text = malloc(size * sizeof(char));
-            if (text == NULL) {
-                fprintf(stderr, "Error in malloc\n");
-            } else {
+    size_t size = 8192;
+    char *text = malloc(size* sizeof(char));
+    if (text == NULL) {
+        fprintf(stderr, "Error in malloc\n");
+    } else {
+        for (int i = 1; i < argc; ++i) {
+            int file = open(argv[i], O_RDONLY);
+            if (file >= 0) {
+                ssize_t count = 0;
                 do {
                     count = read(file, text, size);
-                    for (ssize_t j = 0; j < count; ++j) {
-                        printf("%c", text[j]);
-                    }
-                    if(count<0){
+                    if (count < 0) {
                         perror(argv[i]);
                     }
+                    else {
+                        write(STDOUT_FILENO, text, count);
+                    }
                 } while (count > 0);
-                free(text);
                 close(file);
+            } else {
+                perror(argv[i]);
             }
-        } else {
-            //fprintf(stderr, "Can't open file '%s' with error %d\n", argv[i], errno);
-            perror(argv[i]);
         }
+        free(text);
     }
     return 0;
 }
